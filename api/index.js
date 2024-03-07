472f6c5f6ef5e3c51b0770b6c3bd2aa127a8711a
Keyur Saradva <21dcs107@charusat.edu.in>
2024-03-07 18:45:31 +0530

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// process.env.MONGO
+
+mongoose.connect("mongodb+srv://saradvakeyur:keyur1507@auth.jbkdgmw.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=auth")
+.then(()=>{
+    console.log('connected to MongoDB');
+})
+.catch((err)=>{
+    console.log(err);
+});
 
 const app = express();
 
