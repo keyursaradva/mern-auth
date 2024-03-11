3c800c7bf4d8a426799a00e5b38cf728e98baf55
Keyur Saradva <21dcs107@charusat.edu.in>
2024-03-11 22:47:29 +0530

    complete home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to About!</h1>
+      <p className='mb-4 text-slate-700'>This is a simple Auth App using MERN stack.</p>
+      <p className='mb-4 text-slate-700'>Auth App provide mutliple functionality like <br/>Signin,<br/>SignOut,<br/>DeleteAccout,<br/>SigninUsingGoogle,<br/>UploadProfilePhoto,<br/>UpdateProfile.</p>
+    </div>
+    </div>
   )
 }
